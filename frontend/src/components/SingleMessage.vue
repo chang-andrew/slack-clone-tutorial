22c8a280e19177d08e1d1a52c585e9f44450b6d2
Andrew Chang <chang331006@gmail.com>
2020-11-17 13:55:39 -0600

    add message hover effect

--- frontend/src/components/SingleMessage.vue
+++ frontend/src/components/SingleMessage.vue
@@ -1,5 +1,5 @@
 <template lang='pug'>
-  div.px-4
+  div.px-4.relative(@mouseover="isHovered = true" @mouseleave="isHovered = false" :class="{'bg-gray-100': isHovered}")
     div.flex.items-start.text-sm.mb-3
       img.w-10.h-10.rounded(src="https://i.picsum.photos/id/142/200/200.jpg?hmac=L8yY8tFPavTj32ZpuPiqsLsfWgDvW1jvoJ0ETDOUMGg")
       div.flex-1.ml-3
@@ -7,13 +7,21 @@
           span.font-bold SENDER Name
           span.text-gray-500.text-xs.ml-1 12:50 pm
         span This is a sample dummy message body text!
+      // action buttons
+      SingleMessageActions(v-if="isHovered")
 </template>
 
 <script lang='ts'>
   import Component from 'vue-class-component';
   import Vue from 'vue';
+  import SingleMessageActions from './SingleMessageActions.vue';
 
-  @Component({})
+  @Component({
+    components: {
+      SingleMessageActions
+    }
+  })
   export default class SingleMessage extends Vue {
+    isHovered = false;
   }
 </script>
---
+++ frontend/src/components/SingleMessageActions.vue
@@ -0,0 +1,63 @@
+<template lang='pug'>
+  div.action-placement
+    div.action-btn-container
+      div.action-btn
+        SmileIcon.action-icon
+      div.action-btn
+        EditIcon.action-icon
+      div.action-btn
+        Trash2Icon.action-icon.text-red-500
+</template>
+
+<script lang='ts'>
+  import Component from 'vue-class-component';
+  import Vue from 'vue';
+  import { SmileIcon, EditIcon, Trash2Icon } from 'vue-feather-icons';
+
+  @Component({
+    components: {
+      SmileIcon,
+      EditIcon,
+      Trash2Icon
+    }
+  })
+  export default class SingleMessageActions extends Vue {
+  }
+</script>
+
+<style scoped>
+  .action-placement {
+    @apply absolute;
+    @apply bg-white;
+    top: -16px;
+    right: 27px;
+  }
+
+  .action-btn-container {
+    @apply flex;
+    @apply rounded-lg;
+    @apply border;
+    @apply border-gray-500;
+    padding: 2px;
+  }
+
+  .action-btn {
+    @apply h-8;
+    @apply w-8;
+    @apply rounded-sm;
+    @apply flex;
+    @apply items-center;
+    @apply justify-center;
+    @apply cursor-pointer;
+  }
+
+  .action-btn:hover {
+    @apply bg-gray-200;
+  }
+
+  .action-icon {
+    @apply h-4;
+    @apply w-4;
+  }
+
+</style>
