ce1b204a8eac8afbf330108b18ad4c2196027fbe
Andrew Chang <chang331006@gmail.com>
2020-12-06 16:10:03 -0600

    add transition to modal and background

--- frontend/src/components/util/Modal.vue
+++ frontend/src/components/util/Modal.vue
@@ -1,26 +1,28 @@
 <template lang='pug'>
   div
     // div covering whole screen, uses fixed + inset-0 to take up whole screen. Flex will center the modal within whole screen
-    div.overflow-x-hidden.overflow-y-auto.fixed.inset-0.z-50.justify-center.items-center.flex(v-if="isVisible")
-      // wrapper div for the modal body, helps size the div
-      div.relative.w-auto.my-6.mx-auto.max-w-5xl
-        // content
-        div.rounded-lg.shadow-lg.relative.flex.flex-col.w-full.bg-white
-          // header
-          div.flex.items-start.justify-between.p-5
-            h3.text-2xl.font-semibold
-              p Modal Title
-            button.p-1.ml-auto.float-right.leading-none(@click="emitCancel")
-              div.text-black.h-6.w-6.text-2xl.font-semibold
-                | ×
-          // body
-          div.relative.p-6.flex-auto
-            p Some random text inside of this modal
-          // footer
-          div.flex.items-center.justify-end.p-6
-            button.btn-outline(@click="emitCancel") Cancel
-            button.btn.ml-2 Ok
-    div.opacity-25.fixed.inset-0.z-40.bg-black(v-if="isVisible")
+    transition(name='modal-body')
+      div.overflow-x-hidden.overflow-y-auto.fixed.inset-0.z-50.justify-center.items-center.flex(v-if="isVisible")
+        // wrapper div for the modal body, helps size the div
+        div.relative.w-auto.my-6.mx-auto.max-w-5xl
+          // content
+          div.rounded-lg.shadow-lg.relative.flex.flex-col.w-full.bg-white
+            // header
+            div.flex.items-start.justify-between.p-5
+              h3.text-2xl.font-semibold
+                p Modal Title
+              button.p-1.ml-auto.float-right.leading-none(@click="emitCancel")
+                div.text-black.h-6.w-6.text-2xl.font-semibold
+                  | ×
+            // body
+            div.relative.p-6.flex-auto
+              p Some random text inside of this modal
+            // footer
+            div.flex.items-center.justify-end.p-6
+              button.btn-outline(@click="emitCancel") Cancel
+              button.btn.ml-2 Ok
+    transition(name='modal-background')
+      div.opacity-25.fixed.inset-0.z-40.bg-black(v-if="isVisible")
 </template>
 
 <script lang='ts'>
@@ -37,3 +39,22 @@
     }
   }
 </script>
+
+<style scoped>
+  .modal-body-leave-active, .modal-body-enter-active, .modal-background-leave-active, .modal-background-enter-active {
+    @apply transition-all;
+    @apply duration-200;
+  }
+
+  .modal-body-leave-to, .modal-background-leave-to, .modal-body-enter, .modal-background-enter {
+    opacity: 0;
+  }
+
+  .modal-body-enter-to {
+    opacity: 1;
+  }
+
+  .modal-background-enter-to {
+    opacity: .25;
+  }
+</style>
