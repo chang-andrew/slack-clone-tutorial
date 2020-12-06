b7581be5d53c365ca3ef1d2d6a9d97c6d73f7ca3
Andrew Chang <chang331006@gmail.com>
2020-12-06 15:54:54 -0600

    Modal HTML, on a testing page

---
+++ frontend/src/components/util/Modal.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+  div
+    // div covering whole screen, uses fixed + inset-0 to take up whole screen. Flex will center the modal within whole screen
+    div.overflow-x-hidden.overflow-y-auto.fixed.inset-0.z-50.justify-center.items-center.flex
+      // wrapper div for the modal body, helps size the div
+      div.relative.w-auto.my-6.mx-auto.max-w-5xl
+        // content
+        div.rounded-lg.shadow-lg.relative.flex.flex-col.w-full.bg-white
+          // header
+          div.flex.items-start.justify-between.p-5
+            h3.text-2xl.font-semibold
+              p Modal Title
+            button.p-1.ml-auto.float-right.leading-none
+              div.text-black.h-6.w-6.text-2xl.font-semibold
+                | ×
+          // body
+          div.relative.p-6.flex-auto
+            p Some random text inside of this modal
+          // footer
+          div.flex.items-center.justify-end.p-6
+            button.btn-outline Cancel
+            button.btn.ml-2 Ok
+    div.opacity-25.fixed.inset-0.z-40.bg-black
+</template>
+
+<script lang='ts'>
+  import Component from 'vue-class-component';
+  import Vue from 'vue';
+
+  @Component({})
+  export default class Modal extends Vue {
+  }
+</script>
---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+  div
+    p Just a testing page
+    Modal
+</template>
+
+<script lang='ts'>
+  import Component from 'vue-class-component';
+  import Vue from 'vue';
+  import Modal from '@/components/util/Modal.vue';
+
+  @Component({
+    components: {
+      Modal,
+    }
+  })
+  export default class Test extends Vue {
+  }
+</script>
