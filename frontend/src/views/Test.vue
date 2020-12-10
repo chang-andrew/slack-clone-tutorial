cf9157bad32de1e6edd0d940f54641b88c572df9
Andrew Chang <chang331006@gmail.com>
2020-12-09 21:12:54 -0600

    p1

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -2,16 +2,21 @@
   div.flex.justify-center
     div
       h1.text-4xl Just a testing page
+      button.btn(@click="modalVisible = true") Open Modal
+    Modal(:isVisible="modalVisible" @cancel="modalVisible = false" @confirm="modalVisible = false")
 </template>
 
 <script lang='ts'>
   import Component from 'vue-class-component';
   import Vue from 'vue';
+  import Modal from '@/components/util/Modal.vue';
 
   @Component({
     components: {
+      Modal
     }
   })
   export default class Test extends Vue {
+    modalVisible = false;
   }
 </script>
