49020dd859294ad6c8906613b460f641bfb32704
Andrew Chang <chang331006@gmail.com>
2020-12-18 00:04:01 +0000

    setup example

--- frontend/src/components/util/Carousel.vue
+++ frontend/src/components/util/Carousel.vue
@@ -4,8 +4,7 @@
     div.flex.justify-between.items-center
       button.btn.m-4.h-8(@click="prevPhoto") <
       div.carousel-body
-        transition(name='carousel')
-          img.carousel-img(v-for="p in activePhotos" :src="p.src" :key="p.id")
+        img.carousel-img(:src="activePhotoSrc")
       button.btn.m-4.h-8(@click="nextPhoto") >
 </template>
 
@@ -16,20 +15,14 @@
 
   @Component({})
   export default class Carousel extends Vue {
-    photos: any[] = [
-      {
-        id: 0,
-        src: 'https://i.picsum.photos/id/928/200/200.jpg?hmac=5MQxbf-ANcu87ZaOn5sOEObpZ9PpJfrOImdC7yOkBlg'
-      },
-      {
-        id: 1,
-        src: 'https://i.picsum.photos/id/460/200/200.jpg?hmac=hL3I5G2p0p6vDGPyV9hergug-KipbUJVxqnnGIEBXg4'
-      }
+    photos: string[] = [
+      'https://i.picsum.photos/id/928/200/200.jpg?hmac=5MQxbf-ANcu87ZaOn5sOEObpZ9PpJfrOImdC7yOkBlg',
+      'https://i.picsum.photos/id/460/200/200.jpg?hmac=hL3I5G2p0p6vDGPyV9hergug-KipbUJVxqnnGIEBXg4',
     ]
     activePhotoId = 0;
 
-    get activePhotos(): any[] {
-      return this.photos.filter((p) => p.id === this.activePhotoId);
+    get activePhotoSrc() {
+      return this.photos[this.activePhotoId];
     }
 
     nextPhoto() {
@@ -56,28 +49,5 @@
     position:absolute;
     left: 0;
     bottom: 0;
-  }
-  /* transition classes*/
-  .carousel-leave-active {
-    @apply transition;
-    @apply duration-1000;
-  }
-  .carousel-enter-active {
-    @apply transition;
-    @apply duration-1000;
-    @apply delay-100;
-  }
-  .carousel-enter {
-    @apply opacity-0;
-    @apply transform;
-    @apply translate-x-8;
-  }
-  .carousel-leave-to {
-    @apply opacity-0;
-    @apply transform;
-    @apply -translate-x-4;
-  }
-  .carousel-leave, .carousel-enter-to {
-    @apply opacity-100;
   }
 </style>
