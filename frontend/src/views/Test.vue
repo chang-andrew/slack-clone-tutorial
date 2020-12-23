977dc471badbd9b1c89ddcd941f4a662ff6386d5
Andrew Chang <chang331006@gmail.com>
2020-12-23 20:15:51 +0000

    example setup

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,25 +1,65 @@
 <template lang='pug'>
-  div.flex.justify-center
-    div
-      h1.text-4xl Just a testing page
-      button.btn(@click="modalVisible = true") Open Modal
-      Carousel
-    Modal(:isVisible="modalVisible" @cancel="modalVisible = false" @confirm="modalVisible = false")
+  div.flex.justify-center.mt-10
+    div.list-container
+      div.item(v-for="(item, i) in allItems")
+        div
+          input.mr-2(type='checkbox')
+          span {{ item }}
+        div.action-btn(@click="deleteItem(i)")
+          Trash2Icon.action-icon.text-red-500
 </template>
 
 <script lang='ts'>
   import Component from 'vue-class-component';
   import Vue from 'vue';
-  import Modal from '@/components/util/Modal.vue';
-  import Carousel from '@/components/util/Carousel.vue';
+  import { Trash2Icon } from 'vue-feather-icons';
 
   @Component({
     components: {
-      Modal,
-      Carousel
+      Trash2Icon
     }
   })
   export default class Test extends Vue {
-    modalVisible = false;
+    itemDeleteModalVisible = false;
+    allItems = [
+      'Grocery shopping',
+      'Pick up mail',
+      'Pay bills',
+      'Finish publication draft'
+    ];
+
+    deleteItem(itemIndex: any) {
+      this.allItems.splice(itemIndex, 1);
+    }
   }
 </script>
+
+<style scoped>
+  .list-container {
+    width: 350px;
+    @apply border;
+    @apply border-black;
+    @apply p-4;
+  }
+
+  .item {
+    @apply flex;
+    @apply items-center;
+    @apply justify-between;
+    @apply p-2;
+  }
+
+  .action-btn {
+    @apply h-8;
+    @apply w-8;
+    @apply rounded-sm;
+    @apply flex;
+    @apply items-center;
+    @apply justify-center;
+    @apply cursor-pointer;
+  }
+  .action-icon {
+    @apply h-4;
+    @apply w-4;
+  }
+</style>
